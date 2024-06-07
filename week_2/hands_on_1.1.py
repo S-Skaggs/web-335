70b6f44b8b4585e0989a95ecac43f48662e4a7ee
S-Skaggs <s-skaggs@outlook.com>
2024-06-07 09:13:29 -0400

    FEAT: Completed Hands-On 1.1: Building Your First Python Program

---
+++ week_2/hands_on_1.1.py
@@ -0,0 +1,14 @@
+"""
+Author: Sheldon Skaggs
+Date: 6/7/2024
+File Name: hands_on_1.1.py
+Description: This Python solution will print one of my favorite quotes
+"""
+
+# Set variables to hold data
+quote = "There are 3 things I have learned never to discuss with people: religion, politics, & the Great Pumpkin."
+character = "Linus Van Pelt"
+author = "Charles M. Schulz"
+
+# Print the data in a formatted string
+print(quote + " - " + character + " (" + author + ")")
